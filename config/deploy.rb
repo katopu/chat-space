65cd71bca1882709dfc85bd8045e50975301b888
katopu <kato-d.home@outlook.jp>
2020-07-27 20:39:02 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,12 @@
 lock "3.14.1"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:katopu/chat-space.git"
+set :repo_url, "git@github.com:katopu/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['chatspace1.pem']
+                  keys: ['~/.ssh/chatspace1.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
